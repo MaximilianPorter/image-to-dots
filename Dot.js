036f7a79a8e064595e02cdf9958065d26887ac35
MaximilianPorter <mx.porters@gmail.com>
2023-08-23 00:29:14 -0700

    spatial grid for better performance

--- Dot.js
+++ Dot.js
@@ -1,10 +1,15 @@
 class Dot {
-  constructor(id, velocity, position, radius, collisionRadius) {
+  constructor(id, velocity, position, radius, collisionRadius, cellSize) {
     this.id = id;
     this.velocity = velocity;
     this.position = position;
     this.radius = radius;
     this.collisionRadius = collisionRadius;
+    this.gridPosition = {
+      x: Math.floor(position.x / cellSize),
+      y: Math.floor(position.y / cellSize),
+    };
+    this.cellSize = cellSize;
 
     this.lastPosition = position;
   }
@@ -14,6 +19,11 @@ class Dot {
 
     this.position.x = this.position.x + this.velocity.x;
     this.position.y = this.position.y + this.velocity.y;
+  }
+
+  updateGridPosition() {
+    this.gridPosition.x = Math.floor(this.position.x / this.cellSize);
+    this.gridPosition.y = Math.floor(this.position.y / this.cellSize);
   }
 }
 
--- script.js
+++ script.js
@@ -6,19 +6,21 @@ const canvas = document.getElementById("input-image-preview");
 const canvasContext = canvas.getContext("2d");
 const dotsAreaCanvas = document.getElementById("dots-area");
 const dotsAreaCanvasContext = dotsAreaCanvas.getContext("2d");
-dotsAreaCanvas.width = window.innerWidth;
-dotsAreaCanvas.height = window.innerHeight;
+
+let uploadedImageData;
+const CELL_SIZE = 10;
+const grid = {};
 
 input_img_element.addEventListener("change", (e) => {
   createReader(e.target.files[0], function () {
     // Loop through each pixel
-    const imageData = canvasContext.getImageData(
+    uploadedImageData = canvasContext.getImageData(
       0,
       0,
       canvas.width,
       canvas.height
     );
-    const data = imageData.data; // pixel data in a one-dimensional array
+    const data = uploadedImageData.data; // pixel data in a one-dimensional array
 
     for (let i = 0; i < data.length; i += 4) {
       const red = data[i]; // Red component of the pixel
@@ -36,7 +38,10 @@ input_img_element.addEventListener("change", (e) => {
       data[i + 3] = alpha;
     }
 
-    canvasContext.putImageData(imageData, 0, 0);
+    canvasContext.putImageData(uploadedImageData, 0, 0);
+    addDots();
+    moveDots();
+    // console.log(getApproximatePixelColor(0.5, 0.5, uploadedImageData));
   });
 });
 
@@ -48,6 +53,11 @@ function createReader(file, whenReady) {
       canvas.width = image.width;
       canvas.height = image.height;
 
+      // match aspect ratio of canvas
+      dotsAreaCanvas.width = window.innerWidth;
+      dotsAreaCanvas.height =
+        (window.innerWidth / canvas.width) * canvas.height;
+
       canvasContext.drawImage(image, 0, 0);
 
       if (whenReady) whenReady();
@@ -58,12 +68,16 @@ function createReader(file, whenReady) {
 }
 
 const dotDictionary = {};
-const dotsToAdd = 1000;
-const dotSpeed = 1;
-const maxDotRadius = 5;
-const minDotRadius = 1;
+const dotsToAdd = 10000;
+const dotSpeed = 2;
+const maxDotRadius = 3;
+const minDotRadius = 0.01;
+const bounceDamper = 0.1;
+const acceleration = 0.001;
+const radiusChangeRate = 0.1;
 
 function addDots() {
+  createGrid();
   for (let i = 0; i < dotsToAdd; i++) {
     const pos = {
       x: ((Math.random() * 80 + 10) / 100) * dotsAreaCanvas.width,
@@ -75,16 +89,28 @@ function addDots() {
       y: dotSpeed * (Math.random() < 0.5 ? -1 : 1),
     };
 
-    const dotObject = new Dot(i, velocity, pos, 20, maxDotRadius);
+    const dotObject = new Dot(
+      i,
+      velocity,
+      pos,
+      minDotRadius,
+      maxDotRadius,
+      CELL_SIZE
+    );
+
+    const gridKey = `${dotObject.gridPosition.x},${dotObject.gridPosition.y}`;
+    grid[gridKey].push(dotObject);
 
     dotDictionary[i] = dotObject;
   }
 }
 
-addDots();
-
 // move dots every frame
 function moveDots() {
+  if (Object.keys(dotDictionary).length === 0) {
+    return;
+  }
+
   // clear canvas
   dotsAreaCanvasContext.clearRect(
     0,
@@ -94,14 +120,65 @@ function moveDots() {
   );
 
   for (const [key, value] of Object.entries(dotDictionary)) {
-    const { velocity, position, lastPosition, collisionRadius } = value;
-    let { radius } = value;
+    const { velocity, position, lastPosition } = value;
+    let { radius, collisionRadius, gridPosition } = value;
     value.updatePosition();
 
-    const yPercent = position.y / dotsAreaCanvas.height;
+    const lastGridPosition = { ...gridPosition };
+    value.updateGridPosition();
+    const newGridPosition = { ...gridPosition };
 
-    radius = minDotRadius + (maxDotRadius - minDotRadius) * yPercent;
+    // if the dot has moved to a new grid position
+    if (
+      lastGridPosition.x !== newGridPosition.x ||
+      lastGridPosition.y !== newGridPosition.y
+    ) {
+      // remove dot from old grid position
+      const oldGridKey = `${lastGridPosition.x},${lastGridPosition.y}`;
+      const oldGridArray = grid[oldGridKey];
+      const index = oldGridArray.findIndex((dot) => dot.id === value.id);
+      oldGridArray.splice(index, 1);
 
+      // add dot to new grid position
+      const newGridKey = `${newGridPosition.x},${newGridPosition.y}`;
+      grid[newGridKey]?.push(value) ?? (grid[newGridKey] = [value]);
+    }
+
+    // console.log(velocity);
+    // console.log(VectorMagnitude(velocity.x, velocity.y));
+    if (Math.abs(velocity.x) < dotSpeed) {
+      velocity.x += acceleration * (velocity.x < 0 ? -1 : 1);
+    }
+    if (Math.abs(velocity.y) < dotSpeed) {
+      velocity.y += acceleration * (velocity.y < 0 ? -1 : 1);
+    }
+
+    // if (VectorMagnitude(velocity.x, velocity.y) < dotSpeed) {
+    //   if (velocity.x < 0) {
+    //     velocity.x -= acceleration;
+    //   } else {
+    //     velocity.x += acceleration;
+    //   }
+    //   if (velocity.y < 0) {
+    //     velocity.y -= acceleration;
+    //   } else {
+    //     velocity.y += acceleration;
+    //   }
+    // }
+
+    // const yPercent = position.y / dotsAreaCanvas.height;
+
+    const pixelValue = getPixelValue(
+      position.x / dotsAreaCanvas.width,
+      position.y / dotsAreaCanvas.height,
+      uploadedImageData
+    );
+
+    const desiredRadius =
+      minDotRadius + (maxDotRadius - minDotRadius) * pixelValue;
+    value.radius += (desiredRadius - radius) * radiusChangeRate;
+
+    // this is to debug the collision radius
     // drawCircle(
     //   dotsAreaCanvasContext,
     //   position.x,
@@ -118,74 +195,63 @@ function moveDots() {
       position.y,
       radius,
       "white",
-      "black",
+      null,
       2
     );
 
-    // bounce off other dots
-    for (const [key2, value2] of Object.entries(dotDictionary)) {
-      if (key === key2) continue;
+    const neighboringDots = getDotsInNeighboringCells(value);
 
-      const { position: position2 } = value2;
+    // bounce off other dots
+    // for (const [key2, dot] of Object.entries(dotDictionary)) {
+    for (const dot of neighboringDots) {
+      if (value === dot) continue;
+
+      const { position: position2, radius: radius2 } = dot;
 
       const distance = Math.sqrt(
         (position.x - position2.x) ** 2 + (position.y - position2.y) ** 2
       );
 
-      if (distance < collisionRadius * 2) {
-        const angle = Math.atan2(
-          position2.y - position.y,
-          position2.x - position.x
+      if (distance < collisionRadius + radius2) {
+        const overlap = collisionRadius + radius2 - distance;
+        const direction = NormalizeVector(
+          position.x - position2.x,
+          position.y - position2.y
         );
 
-        const overlap = collisionRadius * 2 - distance;
+        position.x += direction.x * overlap * bounceDamper;
+        position.y += direction.y * overlap * bounceDamper;
 
-        const moveX = overlap * Math.cos(angle);
-        const moveY = overlap * Math.sin(angle);
-
-        position.x -= moveX;
-        position.y -= moveY;
+        position2.x -= direction.x * overlap * bounceDamper;
+        position2.y -= direction.y * overlap * bounceDamper;
 
         const yDiff = position.y - position2.y;
         const xDiff = position.x - position2.x;
 
         // normalize vector for difference between the two dots
-        const length = Math.sqrt(yDiff * yDiff + xDiff * xDiff);
-        const normalized = {
-          x: xDiff / length,
-          y: yDiff / length,
-        };
+        const normalized = NormalizeVector(xDiff, yDiff);
+        // velocity.x = normalized.x * Math.abs(velocity.x) * bounceDamper;
+        // velocity.y = normalized.y * Math.abs(velocity.y) * bounceDamper;
         velocity.x = normalized.x * dotSpeed;
         velocity.y = normalized.y * dotSpeed;
 
-        // if (position2.x > position.x) {
-        //   velocity.x = -Math.abs(velocity.x);
-        // } else {
-        //   velocity.x = Math.abs(velocity.x);
-        // }
-
-        // if (position2.y > position.y) {
-        //   velocity.y = -Math.abs(velocity.y);
-        // } else {
-        //   velocity.y = Math.abs(velocity.y);
-        // }
+        velocity.x *= bounceDamper;
+        velocity.y *= bounceDamper;
       }
     }
 
     // bounce off walls
-    if (
-      position.x - collisionRadius < 0 ||
-      position.x + collisionRadius > dotsAreaCanvas.width
-    ) {
-      velocity.x = -velocity.x;
-    }
+    if (position.x - radius < 0) {
+      velocity.x = Math.abs(velocity.x);
+    } else if (position.x + radius > dotsAreaCanvas.width)
+      velocity.x = -Math.abs(velocity.x);
 
-    if (
-      position.y - collisionRadius < 0 ||
-      position.y + collisionRadius > dotsAreaCanvas.height
-    ) {
-      velocity.y = -velocity.y;
-    }
+    if (position.y - radius < 0) {
+      velocity.y = Math.abs(velocity.y);
+    } else if (position.y + radius > dotsAreaCanvas.height)
+      velocity.y = -Math.abs(velocity.y);
+
+    value.velocity = velocity;
   }
 
   requestAnimationFrame(moveDots);
@@ -204,4 +270,70 @@ function drawCircle(ctx, x, y, radius, fill, stroke, strokeWidth) {
     ctx.strokeStyle = stroke;
     ctx.stroke();
   }
+}
+
+function getApproximatePixelColor(xPercent, yPercent, imageData) {
+  const x = Math.floor(xPercent * imageData.width);
+  const y = Math.floor(yPercent * imageData.height);
+  const index = (y * imageData.width + x) * 4;
+  const red = imageData.data[index];
+  const green = imageData.data[index + 1];
+  const blue = imageData.data[index + 2];
+  const alpha = imageData.data[index + 3];
+  return { red, green, blue, alpha };
+}
+
+function getPixelValue(xPercent, yPercent, imageData) {
+  const x = Math.floor(xPercent * imageData.width);
+  const y = Math.floor(yPercent * imageData.height);
+  const index = (y * imageData.width + x) * 4;
+
+  // return value 0 1
+  return imageData.data[index] / 255;
+}
+
+function NormalizeVector(x, y) {
+  const length = Math.sqrt(x * x + y * y);
+  return {
+    x: x / length,
+    y: y / length,
+  };
+}
+
+function VectorMagnitude(x, y) {
+  return Math.sqrt(x * x + y * y);
+}
+
+function createGrid() {
+  for (let i = 0; i < dotsAreaCanvas.width / CELL_SIZE; i++) {
+    for (let j = 0; j < dotsAreaCanvas.height / CELL_SIZE; j++) {
+      grid[`${i},${j}`] = [];
+    }
+  }
+
+  console.log(grid);
+}
+
+function getDotsInNeighboringCells(dot) {
+  const { gridPosition } = dot;
+  const neighboringCells = [
+    `${gridPosition.x},${gridPosition.y}`,
+    `${gridPosition.x},${gridPosition.y - 1}`,
+    `${gridPosition.x},${gridPosition.y + 1}`,
+    `${gridPosition.x - 1},${gridPosition.y}`,
+    `${gridPosition.x - 1},${gridPosition.y - 1}`,
+    `${gridPosition.x - 1},${gridPosition.y + 1}`,
+    `${gridPosition.x + 1},${gridPosition.y}`,
+    `${gridPosition.x + 1},${gridPosition.y - 1}`,
+    `${gridPosition.x + 1},${gridPosition.y + 1}`,
+  ];
+
+  const dotsInNeighboringCells = [];
+
+  for (const cell of neighboringCells) {
+    const dotsInCell = grid[cell] ?? [];
+    dotsInNeighboringCells.push(...dotsInCell);
+  }
+
+  return dotsInNeighboringCells;
 }
